67c811d85a082d4960b8cd15cf27c3799087f6e5
David Elaine Alt <elaine@flaminghakama.com>
2017-09-30 19:11:24 -0700

    incremental edits

--- Manifest.js
+++ Manifest.js
@@ -92,7 +92,7 @@ function Manifest(manifest) {
 		for ( name in song.scores ) { 
 	 		score = song.scores[name] ; 
 			href = this.getBaseUrl(song) + 
-				score.fileLocation + '/pdf/' + 
+				song.fileLocation + '/pdf/' + 
 				song.filePrefix + score.fileSuffix + '.pdf' ;
 			html += '<li><a target="_blank" href="' + href + '">' + name + "</a></li>\n" ; 
 		}
@@ -111,7 +111,7 @@ function Manifest(manifest) {
 		for ( name in song.parts ) { 
 			part = song.parts[name] ; 
 			href = this.getBaseUrl(song) + 
-				score.fileLocation + '/pdf/' + 
+				song.fileLocation + '/pdf/' + 
 				song.filePrefix + score.fileSuffix + '.pdf' ;
 			html += '<li><a target="_blank" href="' + href + '">' + name + "</a></li>\n" ; 
 		};
