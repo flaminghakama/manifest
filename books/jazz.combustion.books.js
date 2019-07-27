9f6e108c28a0f374a69f97c7d166094aa243c537
David Elaine Alt <elaine@flaminghakama.com>
2019-07-26 21:41:23 -0700

    committing added Jazz Combustion books

---
+++ books/jazz.combustion.books.js
@@ -0,0 +1,15 @@
+/*
+ *  Books in Jazz Combustion Quartet
+ */
+
+var jazzCombustionBooks = {
+	bookOrder: [ '01', '02', '03', '04' ],
+	books: {
+		'01': "Concert Lead Sheets",
+		'02': "Bb Instruments",
+		'03': "Eb Instruments",
+		'04': "Bass"
+	}
+};
+
+
